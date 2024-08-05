a769a2dcf95aed5e7f6f62f321a464190b41ed4f
dcortesnet <dcortes.net@gmail.com>
2024-08-05 18:15:20 -0400

    feat: Update Navbar component to use react-router-dom's useNavigate hook
    This commit modifies the Navbar component to use the useNavigate hook from the react-router-dom library. It adds a handleClick function that prevents the default behavior of the anchor tag and uses the navigate function to redirect to the "/contact" route when the "Contact" link is clicked. This change improves the navigation functionality of the Navbar component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export function Navbar() {
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    navigate("/contact");
+  };
+
   return (
     <ul>
       <li>
         <Link to="/">Home</Link>
       </li>
       <li>
-        <Link to="/contact">Contact</Link>
+        <a href="#" onClick={handleClick}>
+          Contact
+        </a>
       </li>
     </ul>
   );
